d75884657ec91f2e65a7b2d677f976d5f2b53026
kushsingh <kush.singh84@gmail.com>
2013-08-13 12:43:17 +0530

    Zoom Base

---
+++ tools/zoom/zoom.js
@@ -0,0 +1,46 @@
+/**
+ * ZOOM plugin for Jquery 
+ * param @author ksin39
+ */
+var zoom = (function (zoom, $) {
+	
+	/**
+	 * url : image url
+	 */
+	function image_ajax(url){
+		
+	}
+	/**
+	 * el : element selector 
+	 * total : total count of images
+	 * count : current image count 
+	 */
+	function next(el,total,count){
+		
+	}
+	/**
+	 * el : element selector 
+	 * total : total count of images
+	 * count : current image count 
+	 */
+	function prev(id,total,count){
+		
+	}
+	/**
+	 * el : element selector 
+	 * zomX : zoom ratio for x co-ordinate
+	 * zoomY : zoom ratio for y co-ordinate
+	 */
+	function zoom(zomX,zoomY){
+		
+	}
+	/**
+	 * el : element selector 
+	 * total : total count of images
+	 * count : current image count 
+	 */
+	zoom.init = function (el){
+		
+	}
+	
+}(window.zoom = window.zoom || {}, jQuery));
